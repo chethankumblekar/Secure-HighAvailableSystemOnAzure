c089171e11ee459a69db9fc7fd794cba9fdd1e9b
chethankumblekar <kc015800@gmail.com>
2026-02-08 11:11:59 +0530

    using remote backend (terraform cloud) for terraform state

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,6 +1,14 @@
 terraform {
+
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "my-terraform-interview"
+    workspaces { 
+      name = "azure-secure-devops-dev" 
+    } 
+  }
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
