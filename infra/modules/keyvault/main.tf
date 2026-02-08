132336a28b4df64c9c72f1e2b23598a0b462b16a
chethankumblekar <kc015800@gmail.com>
2026-02-08 13:50:09 +0530

    updated keyvault name

--- infra/modules/keyvault/main.tf
+++ infra/modules/keyvault/main.tf
@@ -5,7 +5,7 @@ resource "random_string" "kv_suffix" {
 }
 
 resource "azurerm_key_vault" "this" {
-  name                = "${var.project_name}-${var.environment}-kv-${random_string.kv_suffix.result}"
+  name                = "${var.environment}-kv-${random_string.kv_suffix.result}"
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = var.tenant_id
